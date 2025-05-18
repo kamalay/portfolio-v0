c6734180cf6e2ff3495e48df01aee1d2b8d7ab71
kamalay <kamalay.kishore@gmail.com>
2025-05-18 10:52:33 -0400

    commit

--- src/components/sections/experienceSection.tsx
+++ src/components/sections/experienceSection.tsx
@@ -17,89 +17,220 @@ export default function ExperienceSection() {
 
         {/* Section Content */}
         <div className="mx-auto flex flex-col lg:flex-row gap-6">
-          <div className="lg:w-1/5 bg-bg-secondary-dark rounded-3xl border border-border-dark p-10 space-y-4">
+          <div className="lg:w-1/3 bg-bg-secondary-dark rounded-3xl border border-border-dark p-10 space-y-4">
             <p className="pb-2">
-              I&#39;m a Senior Software Engineer with nearly 10 years of
-              experience in the Software Development Lifecycle (SDLC), I have
-              expertise in core areas such as Analysis, Design, Implementation,
-              Testing, and Deployment of Object-Oriented, Client-Server,
-              Web-Based and Distributed Enterprise Applications. Experience of
-              development using software development methodologies such as
-              Agile, Waterfall development and Test-Driven Development (TDD)
-              environments and Utilized GitHub, JIRA, Bitbucket, Confluence for
-              version control. Skilled in designing and developing Web-centric
-              applications using modern technologies such as HTML5, JavaScript,
-              ReactJS, ExpressJS, Typescript and worked closely with UX
-              designers and leveraged Figma Design tool for Application
-              Development
+              <h1 className="text-xl font-semibold">
+                MassMutual (Boston, MA) (January 2022 - Current)
+              </h1>
+              <p>
+                · Supported multiple MassMutual Institutional projects including
+                Pension risk Transfer, Municipal guaranteed Investments,
+                Insurance portal.
+              </p>{" "}
+              <p>
+                · Coordinated the Agile process of the team, evaluated the
+                development tasks worked with scrum masters and assigning
+                between developers.
+              </p>{" "}
+              <p>
+                · Leveraged Frontend Technologies such as JavaScript, ES6,
+                HTML5, ReactJS, Bootstrap, ExpressJS, CSS3 for development of
+                Frontend Application.{" "}
+              </p>
+              <p>
+                · Extensively used Node.js runtime environment for JavaScript
+                and leveraged NPM for package management and installation.
+              </p>{" "}
+              <p>
+                · Implemented microservice architecture for development and
+                deployment of the project ensuring scalability and modularity.
+              </p>{" "}
+              <p>
+                · Designed and Consumed Restful web services and Tested REST
+                services using POSTMAN and Insomnia Testing Tool.
+              </p>{" "}
+              <p>
+                · Developed core backend functionality using spring boot, Java17
+                and Collections framework.
+              </p>{" "}
+              <p>
+                · Successfully migrated Spring boot microservices from 2.0 to
+                3.0 version ensuring the application is up-to-date with latest
+                framework version.
+              </p>{" "}
+              <p>
+                · Extensively used Spring data JPA and Spring JDBC Template to
+                connect to DBeaver managed Vertica database to fetch records.
+              </p>{" "}
+              <p>
+                · Implemented authentication using Spring Security features and
+                frontend React Authentication, supporting Basic, JWT and
+                OAuth2.0 mechanisms.
+              </p>{" "}
+              <p>
+                · Utilized Akamai and Amazon s3 tool for content management
+                system and implemented Redis for efficient caching.{" "}
+              </p>{" "}
+              <p>
+                · Leveraged Kubernetes, Docker and Helm Charts for development,
+                deployment and orchestration of the application.
+              </p>{" "}
+              · Recognized by SLT for Resolving Security Vulnerabilities and
+              Deprecated java and NPM Dependencies and effectively utilized
+              snyk.
             </p>
           </div>
-          <div className="lg:w-1/5 bg-bg-secondary-dark rounded-3xl border border-border-dark p-10 space-y-4">
+          <div className="lg:w-1/3 bg-bg-secondary-dark rounded-3xl border border-border-dark p-10 space-y-4">
             <p className="pb-2">
-              I&#39;m a Senior Software Engineer with nearly 10 years of
-              experience in the Software Development Lifecycle (SDLC), I have
-              expertise in core areas such as Analysis, Design, Implementation,
-              Testing, and Deployment of Object-Oriented, Client-Server,
-              Web-Based and Distributed Enterprise Applications. Experience of
-              development using software development methodologies such as
-              Agile, Waterfall development and Test-Driven Development (TDD)
-              environments and Utilized GitHub, JIRA, Bitbucket, Confluence for
-              version control. Skilled in designing and developing Web-centric
-              applications using modern technologies such as HTML5, JavaScript,
-              ReactJS, ExpressJS, Typescript and worked closely with UX
-              designers and leveraged Figma Design tool for Application
-              Development
+              <h1 className="text-xl font-semibold">
+                JJILL, Inc (Quincy, MA) ( June 2018 - January 2022)
+              </h1>
+              <p>
+                · I supported JJILL Ecommerce, JJILL was established in 1959 J.
+                Jill began as one specialty store in the Berkshires of
+                Massachusetts and evolved into a Direct catalogue business.
+              </p>{" "}
+              <p>
+                At JJILL I utilized IBM WebSphere commerce for entire back-end
+                development.
+              </p>{" "}
+              <p>
+                · Involved in the Design and Development of project modules,
+                gather requirements using Agile/Scrum Methodology.{" "}
+              </p>{" "}
+              <p>
+                · Defined and developed the application presentation layer in UI
+                development with technologies like JavaScript, ES6, HTML5,
+                ReactJS, MaterialUI.
+              </p>{" "}
+              <p>
+                · Used React front end web development framework Next.js to
+                generate static page for server-side rendering and to achieve
+                enhance SEO.
+              </p>{" "}
+              <p>
+                · Implemented React Hooks like useState, useEffect, useContext,
+                useMemo etc which reduced code complexity and enhanced
+                readability.{" "}
+              </p>{" "}
+              <p>
+                · Completely revamped the Frontend Application using Progressive
+                Web App thus decreased page load time by 30%.
+              </p>{" "}
+              <p>
+                · Implemented the Microservices architecture with Spring Boot
+                based Microservices interacting via RESTFUL Webservices.
+              </p>{" "}
+              <p>
+                · Utilized Java8, spring MVC, spring beans, collections for
+                developments of overall backend code.
+              </p>{" "}
+              <p>
+                · Handled backend code deployments using IBM Urban Code Deploy
+                and leveraged WebSphere Application Server for development
+                activities.
+              </p>{" "}
+              · Proficient in working with HCL commerce an E-commerce platform
+              helps in delivering user experience for B2B, B2C platform.{" "}
+              <p>
+                {" "}
+                · Utilized DB2 designed to store, analyze, and retrieve data
+                efficiently.
+              </p>{" "}
+              <p>
+                · Deployed Moovweb XDN Frontend React application using AWS
+                Lambda serverless execution.{" "}
+              </p>
+              <p>
+                · Utilized tools such as Mpulse, New Relic, Akamai, and Tealeaf
+                for comprehensive website performance tracking and monitoring
+                and served as On-call support for the application, ensuring
+                timely issue resolution.
+              </p>
             </p>
           </div>
-          <div className="lg:w-1/5 bg-bg-secondary-dark rounded-3xl border border-border-dark p-10 space-y-4">
+          <div className="lg:w-1/3 bg-bg-secondary-dark rounded-3xl border border-border-dark p-10 space-y-4">
+            <h1 className="text-xl font-semibold">
+              IBM (SouthBury, CT) ( Nov 2016 - June 2018){" "}
+            </h1>
             <p className="pb-2">
-              I&#39;m a Senior Software Engineer with nearly 10 years of
-              experience in the Software Development Lifecycle (SDLC), I have
-              expertise in core areas such as Analysis, Design, Implementation,
-              Testing, and Deployment of Object-Oriented, Client-Server,
-              Web-Based and Distributed Enterprise Applications. Experience of
-              development using software development methodologies such as
-              Agile, Waterfall development and Test-Driven Development (TDD)
-              environments and Utilized GitHub, JIRA, Bitbucket, Confluence for
-              version control. Skilled in designing and developing Web-centric
-              applications using modern technologies such as HTML5, JavaScript,
-              ReactJS, ExpressJS, Typescript and worked closely with UX
-              designers and leveraged Figma Design tool for Application
-              Development
+              <p>
+                · Support multiple applications under the IBM Finance Systems,
+                Controls Audit, and Licensing division which include WWBCIT,
+                SRM, and LFMS.
+              </p>
+              <p>
+                · Utilized IBM Rational Rose Software and IBM Rational
+                Application Developer as integrated development environment for
+                software design.
+              </p>
+              <p>
+                · Utilized Java, Collections, Multithreading, JSP’s for overall
+                development of the Application.
+              </p>
+              <p>
+                · Leveraged Spring framework and implemented design patterns
+                such as Dependency Injection and Spring DAO for application
+                development.
+              </p>{" "}
+              <p>
+                · Used Maven as a build and project management tool and utilized
+                Hibernate ORM tool.
+              </p>{" "}
+              <p>
+                · Leveraged services like EC2, Cloud Watch, S3, RDS for
+                deployment and monitoring for the Application.{" "}
+              </p>
+              <p>
+                · Leveraged HQL Queries to store and manipulate data with the
+                database.
+              </p>{" "}
+              <p>
+                · Implemented testcases with Junit to verify application
+                behaviour and support test-driven development practises.{" "}
+              </p>
+              <p>
+                · Practiced Agile methodology for overall development of the
+                code.
+              </p>{" "}
+              <p>
+                · Practiced Test Driven Development where we developed the code
+                and then tested the code and then refactored the code.
+              </p>
             </p>
           </div>
-          <div className="lg:w-1/5 bg-bg-secondary-dark rounded-3xl border border-border-dark p-10 space-y-4">
+        </div>
+        <div className="mx-auto flex flex-col lg:flex-row gap-6">
+          <div className="lg:w-1/3 bg-bg-secondary-dark rounded-3xl border border-border-dark p-10 space-y-4">
             <p className="pb-2">
-              I&#39;m a Senior Software Engineer with nearly 10 years of
-              experience in the Software Development Lifecycle (SDLC), I have
-              expertise in core areas such as Analysis, Design, Implementation,
-              Testing, and Deployment of Object-Oriented, Client-Server,
-              Web-Based and Distributed Enterprise Applications. Experience of
-              development using software development methodologies such as
-              Agile, Waterfall development and Test-Driven Development (TDD)
-              environments and Utilized GitHub, JIRA, Bitbucket, Confluence for
-              version control. Skilled in designing and developing Web-centric
-              applications using modern technologies such as HTML5, JavaScript,
-              ReactJS, ExpressJS, Typescript and worked closely with UX
-              designers and leveraged Figma Design tool for Application
-              Development
+              · Was responsible for developing the UI pages using HTML5, CSS3,
+              JavaScript, Bootstrap, JSON, Nodejs. · Utilized CSS3 code for the
+              front-end styling of the pages. · Worked with ECMA Script 6
+              features. For the JSX and ES2015(ES6) used Babel, web pack. ·
+              Utilized spring MVC, spring beans, collections for developments of
+              overall backend code. · Created model layer using Hibernate and
+              extensively used its API to perform crud operation. · Worked Web
+              Services composition in by using JAXB API, JAX-RS and JAX-WS
+              Services. · Deep understanding and working experience in Object
+              Oriented Java · Tested REST services using google chromes POSTMAN
+              plugin. Environment: ReactJS, Java, Restful Web services, POSTMAN,
+              JIRA, JSON, HTML5, CSS3, JavaScript, DB2, Agile Methodology.
             </p>
           </div>
-          <div className="lg:w-1/5 bg-bg-secondary-dark rounded-3xl border border-border-dark p-10 space-y-4">
+          <div className="lg:w-1/3 bg-bg-secondary-dark rounded-3xl border border-border-dark p-10 space-y-4">
             <p className="pb-2">
-              I&#39;m a Senior Software Engineer with nearly 10 years of
-              experience in the Software Development Lifecycle (SDLC), I have
-              expertise in core areas such as Analysis, Design, Implementation,
-              Testing, and Deployment of Object-Oriented, Client-Server,
-              Web-Based and Distributed Enterprise Applications. Experience of
-              development using software development methodologies such as
-              Agile, Waterfall development and Test-Driven Development (TDD)
-              environments and Utilized GitHub, JIRA, Bitbucket, Confluence for
-              version control. Skilled in designing and developing Web-centric
-              applications using modern technologies such as HTML5, JavaScript,
-              ReactJS, ExpressJS, Typescript and worked closely with UX
-              designers and leveraged Figma Design tool for Application
-              Development
+              · Responsible and mentored the team in complete software
+              development lifecycle (SDLC) tasks - design, coding, testing, and
+              documentation using Rational Unified Process (RUP) for analysis
+              and design of application. · Designed and developed the web-tier
+              using HTML, JSPs, JavaScript, css3 and Tiles framework. ·
+              Hibernate framework is used in persistence layer for mapping an
+              object-oriented domain model to a relational database (Oracle). ·
+              Used jQuery for faster load of pages and used events to manipulate
+              the elements depends on the user interactions. · Used SQL
+              statements and procedures to fetch the data from the database.
+              Environment: Java, J2EE, Servlets, JSP, XML, DOM, HTML, Tomcat,
+              PL/SQL, JDBC.
             </p>
           </div>
         </div>
